086057e73252d2d93ffcd0b039bb4855ff375842
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2022-12-20 15:14:56 -0300

    ex042

--- Exercicios/ex042.cpp
+++ Exercicios/ex042.cpp
@@ -11,3 +11,45 @@ Se a pessoa responder positivamente a 2 questões ela deve ser classificada como
 #include <cmath>
 #include <cstring>
 #include <locale.h>
+
+int main(){
+    char telefone, local, mora, devia, trabalho;
+    int positivo = 0;
+    printf("Telefonou para a vitima? Responda com s ou n \n");
+    scanf("%c", &telefone);
+        if(telefone == 's' || telefone == 'n'){
+            positivo ++;
+        }
+
+    printf("Esteve no local do crime? Responda com s ou n\n");
+    scanf("%c", &local);
+        if(local == 's' || local== 'n'){
+            positivo ++;
+        }
+
+    printf("Mora perto da vitima? Responda com s ou n\n");
+    scanf("%c", &mora);
+        if(mora == 's' || mora == 'n'){
+            positivo ++;
+        }
+
+    printf("Devia para a vitima? Responda com s ou n\n");
+    scanf("%c", &devia);
+        if(devia == 's' || devia == 'n'){
+            positivo ++;
+        }
+
+    printf("Já trabalhou com a vitima? Responda com s ou n\n");
+    scanf("%c", &trabalho);
+        if(trabalho == 's' || trabalho == 'n'){
+            positivo ++;
+        }
+    if(positivo > 2){
+        printf("Suspeita");
+    } else if(positivo > 3 || positivo < 5) {
+        printf("Cúmplice");
+    } else{
+        printf("Assassino");
+    }
+
+}
