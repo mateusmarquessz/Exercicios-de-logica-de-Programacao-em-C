098db517509321f92bf28c3301d95fb6d1e7b166
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2022-12-25 20:13:12 -0300

    aa

--- Exercicios/ex045.cpp
+++ Exercicios/ex045.cpp
@@ -24,3 +24,43 @@ usuário e gere um cupom fiscal, contendo as informações da compra:
 #include <cstring>
 #include <ctype.h>
 #include <stdio.h>
+
+int main(){
+    char carne, pagamento;
+    float quantidade, preco, precoTotal;
+    printf("Diga o tipo de carne! A-Alcatra, P-Picanha, F-File Duplo");
+    scanf("%s", &carne);
+    printf("Diga a quantidade de carne em KG!");
+    scanf("%f", &quantidade);
+    getchar();
+    printf("Dinheiro ou cartao tabajara (5 porcento de desconto)? D ou C: ");
+    scanf("%s", &pagamento);
+    getchar();
+
+    if(carne == 'A' || carne == 'a'){
+        preco = 5.90;
+    }else if(carne == 'P' || carne == 'p'){
+        preco = 6.90;
+    } else {
+        preco = 4.90;
+    }
+    precoTotal = preco * quantidade;
+    if(carne > 5){
+        preco = preco + 1;
+    } else {
+        preco = preco;
+    }
+
+    if(pagamento == 'C' || pagamento == 'c'){
+        precoTotal = preco / 100 * 5;
+    } else{
+        precoTotal = precoTotal;
+    }
+
+    printf("tipo de carne %s \n", carne);
+    printf("quantidade de carne %f\n", quantidade);
+    printf("Preco total: %f\n", preco);
+    printf("Tipo de pagamento: %s\n", pagamento);
+    printf("valor do desconto %f\n");
+    printf("valor a pagar: %f", precoTotal);
+}
