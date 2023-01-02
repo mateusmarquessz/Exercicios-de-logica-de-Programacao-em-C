f5cb79fbc3c22660d261c26fb5e8b756e8300714
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-01-02 16:01:29 -0300

    att

---
+++ Exercicios/ex051.cpp
@@ -0,0 +1,17 @@
+/*
+Faça um programa que leia 5 números e informe o maior número.
+*/
+#include <cmath>
+#include <cstring>
+#include <ctype.h>
+#include <stdio.h>
+
+int main(){
+    float num1;
+    int i;
+    for(i = 1; i<= 5; i++){ 
+    printf("Digite um numero");
+    scanf("%f", &num1);
+        for()
+    }
+}
