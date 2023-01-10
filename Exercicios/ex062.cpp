c4d829f871fae434263be173ded6c1f503e8c36c
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-01-10 10:45:33 -0300

    att

--- Exercicios/ex062.cpp
+++ Exercicios/ex062.cpp
@@ -7,12 +7,17 @@ Faça um programa que, dado um conjunto de N números, determine o menor valor, 
 #include <stdio.h>
 
 int main(){
-    float n, soma;
+    float n, soma, maior_numero = -INFINITY, nenor_numero = INFINITY;
     printf("Digite um numero qualquer ou 0 para parar e exibir os resultados:");
         scanf("%f", &n);
     while(n>0 || n<0){
         printf("Digite um numero qualquer ou 0 para parar e exibir os resultados:");
         scanf("%f", &n);
+    if(n > maior_numero){
+        maior_numero = n;
+    } else {
+        nenor_numero = n;
     }
-    printf("Menor: %i, Maior: %i, Soma:%i", soma);
+    }
+    printf("Menor: %i, Maior: %i, Soma:%i",nenor_numero,maior_numero,soma);
 }
