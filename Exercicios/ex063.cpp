d2b7145a43883732245da3a1008b8217e168a9e8
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-01-15 20:40:19 -0300

    att

--- Exercicios/ex063.cpp
+++ Exercicios/ex063.cpp
@@ -1,5 +1,5 @@
 /*
-Faça um programa que, dado um conjunto de N números, determine o menor valor, o maior valor e a soma dos valores.
+Altere o programa anterior para que ele aceite apenas números entre 0 e 1000.
 */
 #include <cmath>
 #include <cstring>
